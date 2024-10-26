bfbb99166cbebf24aec844c47c0347e50af31d62
PABLO VERA LOPEZ <pabverlop1@alum.us.es>
2024-10-26 20:24:16 +0200

    fix: refactor url apis

--- backend/index.js
+++ backend/index.js
@@ -6,29 +6,29 @@ const app = express();
 
 app.use(cors());
 
-app.get('/api/categories', async (req, res) => {
+app.get('/api/v1/categories', async (req, res) => {
   try {
-    const response = await fetch('https://tienda.mercadona.es/api/categories/');
+    const response = await fetch('https://tienda.mercadona.es/api/v1/categories/');
     const data = await response.json();
     res.json(data);
   } catch (error) {
     res.status(500).json({ message: 'Error fetching data', error });
   }
 });
-app.get('/api/categories/:id', async (req, res) => {
+app.get('/api/v1/categories/:id', async (req, res) => {
     try {
         const { id } = req.params;
-      const response = await fetch('https://tienda.mercadona.es/api/categories/' + id);
+      const response = await fetch('https://tienda.mercadona.es/api/v1/categories/' + id);
       const data = await response.json();
       res.json(data);
     } catch (error) {
       res.status(500).json({ message: 'Error fetching data', error });
     }
   });
-  app.get('/api/products/:id', async (req, res) => {
+  app.get('/api/v1/products/:id', async (req, res) => {
     try {
         const { id } = req.params;
-      const response = await fetch('https://tienda.mercadona.es/api/products/' + id);
+      const response = await fetch('https://tienda.mercadona.es/api/v1/products/' + id);
       const data = await response.json();
       console.log(data);
       res.json(data);
@@ -37,11 +37,11 @@ app.get('/api/categories/:id', async (req, res) => {
     }
   });
 
-  app.get('/api/nutritional-info/:id', async (req, res) => {
+  app.get('/api/v1/nutritional-info/:id', async (req, res) => {
     try {
       const { id } = req.params;
 
-      const productResponse = await fetch(`https://tienda.mercadona.es/api/products/${id}`);
+      const productResponse = await fetch(`https://tienda.mercadona.es/api/v1/products/${id}`);
       const productData = await productResponse.json();
       if (!productData || !productData.ean) {
         return res.status(404).json({ message: 'Product not found or does not have EAN.' });
@@ -49,7 +49,7 @@ app.get('/api/categories/:id', async (req, res) => {
 
       const ean = productData.ean;
       console.log(ean);
-      const nutritionalResponse = await fetch(`https://world.openfoodfacts.org/api/v0/product/${ean}.json`);
+      const nutritionalResponse = await fetch(`https://world.openfoodfacts.org/api/v1/v0/product/${ean}.json`);
       const nutritionalData = await nutritionalResponse.json();
   
       if (!nutritionalData || nutritionalData.status !== 1) {
